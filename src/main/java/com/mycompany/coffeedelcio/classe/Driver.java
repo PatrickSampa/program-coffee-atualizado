0d36746adfad0274d87429381b648385a92f2706
Davy Costa <113806901+DavyCosta701@users.noreply.github.com>
2023-01-20 11:17:43 -0300

    Update Driver.java

--- src/main/java/com/mycompany/coffeedelcio/classe/Driver.java
+++ src/main/java/com/mycompany/coffeedelcio/classe/Driver.java
@@ -5,7 +5,7 @@ import java.sql.*;
 
 public class Driver {
     public static Statement createConnection() throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3308/starbucks", "root", "3223");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3308/starbucks", "root", "xyzh");
         return connection.createStatement();
     }
 
